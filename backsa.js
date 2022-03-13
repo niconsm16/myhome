df50e49215fab2342aea96bf1f2c816efeb9249a
niconsm <supercuate16@gmail.com>
2022-03-13 06:51:24 -0300

    fix mq js

--- backsa.js
+++ backsa.js
@@ -1,12 +1,11 @@
+let climback = document.querySelector('body .clima')
+let mediaqueryList = window.matchMedia("(max-width: 600px)")
+
 fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 
     .then((response) => response.json())
     .then((a) => {
         setTimeout(() => {
-
-
-            let climback = document.querySelector('body .clima')
-            let mediaqueryList = window.matchMedia("(max-width: 600px)")
 
             mediaqueryList.addEventListener('change', () => {
                 mediaqueryList.matches
