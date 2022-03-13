d658a94c2e53fe5f1844a4a464505d8417d66716
niconsm <supercuate16@gmail.com>
2022-03-13 19:46:36 -0300

    fix back time

--- backsa.js
+++ backsa.js
@@ -1,20 +1,36 @@
 let climback = document.querySelector('body .clima')
 let mediaqueryList = window.matchMedia("(max-width: 600px)")
 
-fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+
+let response = fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 
     .then((response) => response.json())
     .then((a) => {
-        setTimeout(() => {
 
+        if (window.innerWidth <= 600) { climback.style.backgroundImage = `url(${a.url})` }
 
+        mediaqueryList.addEventListener('change', () => {
 
-            mediaqueryList.addEventListener('change', () => {
+            mediaqueryList.matches
+                ? climback.style.backgroundImage = `url(${a.url})`
+                : climback.style.backgroundImage = 'none'
+        })
+    })
 
-                mediaqueryList.matches
-                    ? climback.style.backgroundImage = `url(${a.url})`
-                    : climback.style.backgroundImage = 'none'
-            })
-        }, 100);
+    .catch((error) => {
+
+        console.log('no lo toma')
+
+        if (window.innerWidth <= 600) {
+            climback.style.backgroundImage = 'url("https://picsum.photos/seed/picsum/600/300")'
+        }
+
+        mediaqueryList.addEventListener('change', () => {
+
+            mediaqueryList.matches
+                ? climback.style.backgroundImage = 'url("https://picsum.photos/seed/picsum/600/300")'
+                : climback.style.backgroundImage = 'none'
+        })
+
     })
 
