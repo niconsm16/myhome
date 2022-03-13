370c86a8bc0238730b4c7fbe72820dd0d271ba46
niconsm <supercuate16@gmail.com>
2022-03-13 06:38:28 -0300

    fix mq js

--- backsa.js
+++ backsa.js
@@ -2,15 +2,17 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 
     .then((response) => response.json())
     .then((a) => {
+        setTimeout(() => {
 
-        let climback = document.querySelector('body .clima')
-        let mediaqueryList = window.matchMedia("(max-width: 600px)")
 
-        mediaqueryList.addEventListener('change', () => {
-            mediaqueryList.matches
-                ? climback.style.backgroundImage = `url(${a.url})`
-                : climback.style.backgroundImage = 'none'
-        })
+            let climback = document.querySelector('body .clima')
+            let mediaqueryList = window.matchMedia("(max-width: 600px)")
 
+            mediaqueryList.addEventListener('change', () => {
+                mediaqueryList.matches
+                    ? climback.style.backgroundImage = `url(${a.url})`
+                    : climback.style.backgroundImage = 'none'
+            })
+        }, 1500);
     })
 
